95bdef8b3ce58be562c48b755e3c8d44ee4eeb03
Minh Vương <hardingadonis@gmail.com>
2024-09-06 21:59:39 +0700

    add: implement api docs for `/quizzes`

--- src/routes/quiz.route.ts
+++ src/routes/quiz.route.ts
@@ -9,13 +9,171 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * definitions:
+ *   Quiz:
+ *     type: object
+ *     properties:
+ *       _id:
+ *         type: string
+ *       title:
+ *         type: string
+ *       questions:
+ *         type: array
+ *         items:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   CreateQuizQuery:
+ *     type: object
+ *     properties:
+ *       title:
+ *         type: string
+ *       questions:
+ *         type: array
+ *         items:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   UpdateQuizQuery:
+ *     type: object
+ *     properties:
+ *       title:
+ *         type: string
+ *       questions:
+ *         type: array
+ *         items:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /quizzes:
+ *   get:
+ *     summary: Get all quizzes
+ *     description: Retrieve a list of all quizzes
+ *     tags:
+ *       - Quiz
+ *     responses:
+ *       200:
+ *         description: A list of quizzes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/definitions/Quiz'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
 router.get('/', getAllQuizzes);
+
+/**
+ * @swagger
+ * /quizzes/{quizId}:
+ *   get:
+ *     summary: Get a quiz by ID
+ *     description: Retrieve a quiz by ID
+ *     tags:
+ *       - Quiz
+ *     parameters:
+ *       - in: path
+ *         name: quizId
+ *         required: true
+ *         description: ID of the quiz
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A quiz
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Quiz'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
 router.get('/:quizId', getQuizById);
 
+/**
+ * @swagger
+ * /quizzes:
+ *   post:
+ *     summary: Create a new quiz
+ *     description: Create a new quiz
+ *     tags:
+ *       - Quiz
+ *     parameters:
+ *       - in: body
+ *         name: quiz
+ *         description: The quiz to create
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/CreateQuizQuery'
+ */
 router.post('/', createQuiz);
 
+/**
+ * @swagger
+ * /quizzes/{quizId}:
+ *   put:
+ *     summary: Update a quiz by ID
+ *     description: Update a quiz by ID
+ *     tags:
+ *       - Quiz
+ *     parameters:
+ *       - in: path
+ *         name: quizId
+ *         required: true
+ *         description: ID of the quiz
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: quiz
+ *         description: The quiz to update
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/UpdateQuizQuery'
+ */
 router.put('/:quizId', updateQuiz);
 
+/**
+ * @swagger
+ * /quizzes/{quizId}:
+ *   delete:
+ *     summary: Delete a quiz by ID
+ *     description: Delete a quiz by ID
+ *     tags:
+ *       - Quiz
+ *     parameters:
+ *       - in: path
+ *         name: quizId
+ *         required: true
+ *         description: ID of the quiz
+ *         schema:
+ *           type: string
+ */
 router.delete('/:quizId', deleteQuiz);
 
 export default router;
